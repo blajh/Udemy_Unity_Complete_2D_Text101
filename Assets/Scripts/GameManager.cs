ed3551c5ab765ccbb1c1003dce11bc850027b4ea
Andrej <glazba@gmail.com>
2020-10-26 21:43:37 +0100

    created SO text template

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+	[SerializeField]
+	private TMP_Text storyText;
+
+    void Start()
+    {
+        storyText.text =	"Operation System v.042 loaded...\n" +
+							"User account confirmed...\n" +
+							"Full root access... GRANTED!\n" +
+							".......loading..............\n" +
+							"Dependency packages intalled...\n" +
+							"Link to IP address established...\n";
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="state")]
+public class State : ScriptableObject
+{
+	[TextArea(10,14)]
+	[SerializeField]
+	private string storyText;
+
+
+
+}
